20ee59f0f3616e2302971a61a9428cc690048c3c
Nan Luo <1410314963@qq.com>
2021-06-13 11:02:08 +0800

    init

---
+++ src/steps-test.vue
@@ -0,0 +1,32 @@
+
+<template>
+  <div>
+    <steps :count="count" :active="active"></steps>
+    <button @click="next">下一步</button>
+  </div>
+</template>
+
+<script>
+import Steps from './Steps.vue'
+export default {
+  components: {
+    Steps
+  },
+  data () {
+    return {
+      count: 7,
+      active: 0
+    }
+  },
+  methods: {
+    next () {
+      if (this.active == this.count) return
+      this.active++
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
