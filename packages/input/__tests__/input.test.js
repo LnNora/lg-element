53f0242a090795887efe718d1682e2045a41af4e
Nan Luo <1410314963@qq.com>
2021-06-14 17:09:40 +0800

    rollup打包

--- packages/input/__tests__/input.test.js
+++ packages/input/__tests__/input.test.js
@@ -9,4 +9,33 @@ describe('lg-input', () => {
     const wrapper = mount(input)
     expect(wrapper.html()).toContain('input type="text"')
   })
+
+  test('input-password', () => {
+    const wrapper = mount(input, {
+      propsData: {
+        type: 'password'
+      }
+    })
+    expect(wrapper.html()).toContain('input type="password"')
+  })
+
+  test('input-password', () => {
+    const wrapper = mount(input, {
+      propsData: {
+        type: 'password',
+        value: 'admin'
+      }
+    })
+    expect(wrapper.props('value')).toBe('admin')
+  })
+
+  test('input-snapshot', () => {
+    const wrapper = mount(input, {
+      propsData: {
+        type: 'text',
+        value: 'admin'
+      }
+    })
+    expect(wrapper.vm.$el).toMatchSnapshot()
+  })
 })
